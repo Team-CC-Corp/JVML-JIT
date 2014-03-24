ae03cd4c15dfebb38855dd154bf262329e1f2d9b
ElvishJerricco <elvishjerricco@gmail.com>
2014-03-24 05:44:58 -0400

    Some extra test-casing

--- Test.java
+++ Test.java
@@ -1,9 +1,30 @@
+class A {
+    public void m(){
+        System.out.println("A.m");
+    }
+}
+
+class B extends A {
+    @Override
+    public void m() {
+        super.m();
+        System.out.println("B.m");
+    }
+
+    public void v() {
+        m();
+    }
+}
+
 public class Test {
     public static void main(String[] args) {
         //Integer intt = new Integer(0xBEEFBABE);
         //Object[] ints = new Object[4];
         //ints[0] = "swag";
         Test test = new Test();
+        A obj = new B();
+        obj.m();
+        ((B) obj).v();
     }
 
     public Test() {
