d6b77bff5d7a56333ebb6ff9e55f04806d48398f
ElvishJerricco <elvishjerricco@gmail.com>
2014-06-23 16:19:56 -0400

    Better test_jvml
    Takes arguments so you can specify which classes to test. Also has some
    messages about each test

--- bin/test_jvml.lua
+++ bin/test_jvml.lua
@@ -1,3 +1,4 @@
+local args = {...}
 local jcd = shell.resolve(fs.combine(fs.getDir(shell.getRunningProgram()), ".."))
 local program = fs.combine(jcd, "bin/jvml")
 local tests = fs.combine(jcd, "tests/build")
@@ -5,18 +6,42 @@ local tests = fs.combine(jcd, "tests/build")
 shell.run(program, "-cp", tests, "-g", "-d")
 local vm = jvml.popVM()
 
-for i,v in ipairs(fs.list(tests)) do
-	if v:sub(-6) == ".class" and not v:find("^%.") then
-		v = v:sub(1, -7)
-		local jArray = vm.newArray(vm.getArrayClass("[Ljava.lang.String;"), 0)
-	    local m = vm.findMethod(vm.classByName(v), "main([Ljava/lang/String;)V")
-	    if m then
-		    local ok, err = pcall(m[1], jArray)
-		    if not ok then
-		    	printError(err)
-		        vm.printStackTrace(true)
-		        return
-		    end
+local testsToRun
+
+if #args > 0 then
+	testsToRun = args
+else
+	testsToRun = {}
+	for i,v in ipairs(fs.list(tests)) do
+		if v:sub(-6) == ".class" and not v:find("^%.") then
+			table.insert(testsToRun, v:sub(1,-7))
 		end
+	end
+end
+
+for i,v in ipairs(testsToRun) do
+	local jArray = vm.newArray(vm.getArrayClass("[Ljava.lang.String;"), 0)
+    local m = vm.findMethod(vm.classByName(v), "main([Ljava/lang/String;)V")
+    if m then
+		term.setTextColor(colors.yellow)
+		print(v)
+		term.setTextColor(colors.white)
+	    local ok, err = pcall(m[1], jArray)
+        print()
+	    if not ok then
+	    	printError(err)
+	    	print()
+	        vm.printStackTrace(true)
+	        print()
+
+	        term.setTextColor(colors.orange)
+	        print(v, " Failed")
+	        term.setTextColor(colors.white)
+	        return
+	    else
+	    	term.setTextColor(colors.lime)
+	    	print(v, " Succeded\n")
+	    	term.setTextColor(colors.white)
+	    end
 	end
 end
