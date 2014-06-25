66c053526b750688df782e2558e85286928f0048
ElvishJerricco <elvishjerricco@gmail.com>
2014-06-24 22:07:44 -0400

    RedstoneTest a little better

--- tests/src/RedstoneTest.java
+++ tests/src/RedstoneTest.java
@@ -1,11 +1,13 @@
 import cc.redstone.IRedstone;
 import cc.redstone.SidedRedstone;
+import cc.Computer;
 
 public class RedstoneTest {
 	public static void main(String[] args) {
 		IRedstone rs = new SidedRedstone("back");
 		rs.setOutput(true);
 		System.out.println(rs.getOutput());
+		Computer.sleep(1);
 		rs.setOutput(false);
 		System.out.println(rs.getOutput());
 	}
