0bcda0d7870564b9c957a7ca4df64b462efd3fff
ElvishJerricco <elvishjerricco@gmail.com>
2014-09-16 06:55:51 -0400

    NoSuchFieldError
    Apparently the compiler wants this in some cases for some reason.

---
+++ CCLib/src/java/lang/IncompatibleClassChangeError.java
@@ -0,0 +1,11 @@
+package java.lang;
+
+public class IncompatibleClassChangeError extends LinkageError {
+    public IncompatibleClassChangeError () {
+        super();
+    }
+
+    public IncompatibleClassChangeError(String s) {
+        super(s);
+    }
+}
---
+++ CCLib/src/java/lang/NoSuchFieldError.java
@@ -0,0 +1,10 @@
+package java.lang;
+
+public class NoSuchFieldError extends IncompatibleClassChangeError {
+    public NoSuchFieldError() {
+        super();
+    }
+    public NoSuchFieldError(String s) {
+        super(s);
+    }
+}
