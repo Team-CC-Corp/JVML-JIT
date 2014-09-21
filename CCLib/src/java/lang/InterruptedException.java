b7f60d7546dab590c6920826583ad26608ec0a92
ElvishJerricco <elvishjerricco@gmail.com>
2014-09-21 18:26:35 -0400

    Compiler wants these for try with resource

---
+++ CCLib/src/java/lang/InterruptedException.java
@@ -0,0 +1,11 @@
+package java.lang;
+
+public class InterruptedException extends Exception {
+	public InterruptedException() {
+		super();
+	}
+
+	public InterruptedException(String s) {
+		super(s);
+	}
+}
--- CCLib/src/java/lang/Throwable.java
+++ CCLib/src/java/lang/Throwable.java
@@ -1,11 +1,16 @@
 package java.lang;
 
 import java.io.PrintStream;
+import java.util.ArrayList;
 
 public class Throwable {
+    private static final String CAUSE_CAPTION = "Caused by: ";
+    private static final String SUPPRESSED_CAPTION = "Suppressed: ";
+
     private String detailMessage;
     private Throwable cause = this;
     private StackTraceElement stackTrace[];
+    private ArrayList<Throwable> suppressedExceptions;
 
 	public Throwable() {
 		fillInStackTrace();
@@ -46,10 +51,60 @@ public class Throwable {
         printStackTrace(System.err);
     }
 
-    public void printStackTrace(PrintStream out) {
-    	out.println(this);
-        for (int i = stackTrace.length - 1; i >= 0; --i) {
-            out.println("\tat " + stackTrace[i].toString());
+    private void printStackTrace(PrintStream s) {
+        ArrayList<Throwable> dejaVu = new ArrayList<Throwable>();
+        dejaVu.add(this);
+
+        // Print our stack trace
+        s.println(this);
+        StackTraceElement[] trace = getStackTrace();
+        for (StackTraceElement traceElement : trace)
+            s.println("\tat " + traceElement);
+
+        // Print suppressed exceptions, if any
+        for (Throwable se : getSuppressed())
+            se.printEnclosedStackTrace(s, trace, SUPPRESSED_CAPTION, "\t", dejaVu);
+
+        // Print cause, if any
+        Throwable ourCause = getCause();
+        if (ourCause != null)
+            ourCause.printEnclosedStackTrace(s, trace, CAUSE_CAPTION, "", dejaVu);
+    }
+
+    private void printEnclosedStackTrace(PrintStream s,
+                                         StackTraceElement[] enclosingTrace,
+                                         String caption,
+                                         String prefix,
+                                         ArrayList<Throwable> dejaVu) {
+        if (dejaVu.contains(this)) {
+            s.println("\t[CIRCULAR REFERENCE:" + this + "]");
+        } else {
+            dejaVu.add(this);
+            // Common frames between this and enclosing trace
+            StackTraceElement[] trace = getStackTrace();
+            int m = trace.length - 1;
+            int n = enclosingTrace.length - 1;
+            while (m >= 0 && n >=0 && trace[m].equals(enclosingTrace[n])) {
+                m--; n--;
+            }
+            int framesInCommon = trace.length - 1 - m;
+
+            // Print stack trace
+            s.println(prefix + caption + this);
+            for (int i = 0; i <= m; i++)
+                s.println(prefix + "\tat " + trace[i]);
+            if (framesInCommon != 0)
+                s.println(prefix + "\t... " + framesInCommon + " more");
+
+            // Suppressed exceptions
+            for (Throwable se : getSuppressed())
+                se.printEnclosedStackTrace(s, trace, SUPPRESSED_CAPTION,
+                                           prefix +"\t", dejaVu);
+
+            // Print cause, if any
+            Throwable ourCause = getCause();
+            if (ourCause != null)
+                ourCause.printEnclosedStackTrace(s, trace, CAUSE_CAPTION, prefix, dejaVu);
         }
     }
 
@@ -61,5 +116,23 @@ public class Throwable {
     		stackTraceCopy[i] = stackTrace[i];
     	}
     	return stackTraceCopy;
+    }
+
+    public final Throwable[] getSuppressed() {
+        if (suppressedExceptions == null) {
+            return new Throwable[0];
+        }
+        Throwable[] suppressed = new Throwable[suppressedExceptions.size()];
+        for (int i = 0; i < suppressedExceptions.size(); ++i) {
+            suppressed[i] = suppressedExceptions.get(i);
+        }
+        return suppressed;
+    }
+
+    public final void addSuppressed(Throwable exception) {
+        if (suppressedExceptions == null) {
+            suppressedExceptions = new ArrayList<>();
+        }
+        suppressedExceptions.add(exception);
     }
 }
