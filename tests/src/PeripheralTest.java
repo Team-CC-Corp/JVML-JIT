d6803260b42b2d2e6e9c70741bac4773172c205b
ElvishJerricco <elvishjerricco@gmail.com>
2014-09-22 21:29:50 -0400

    Redid PeripheralTest again.

--- tests/src/PeripheralTest.java
+++ tests/src/PeripheralTest.java
@@ -1,15 +1,20 @@
 import cc.terminal.MonitorTerminal;
+import cc.terminal.TerminalOutputStream;
 import cc.terminal.Color;
 import cc.peripheral.PeripheralNotFoundException;
+import java.io.PrintStream;
+import java.io.IOException;
 
 public class PeripheralTest {
 	public static void main(String[] args) throws PeripheralNotFoundException {
-		MonitorTerminal m = new MonitorTerminal("left");
-		m.setTextColor(Color.GREEN);
-		m.write("Green ");
-		m.setTextColor(Color.BLUE);
-		m.write("Blue ");
-		m.setTextColor(Color.WHITE);
-		m.write("White");
+		TerminalOutputStream termOut = new TerminalOutputStream(new MonitorTerminal("left"));
+		PrintStream monitorPrinter = new PrintStream(termOut, true);
+
+		termOut.setTextColor(Color.RED);
+		monitorPrinter.print("R");
+		termOut.setTextColor(Color.GREEN);
+		monitorPrinter.print("G");
+		termOut.setTextColor(Color.BLUE);
+		monitorPrinter.print("B");
 	}
 }
