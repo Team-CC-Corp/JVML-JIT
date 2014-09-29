cd25a6e05800eef48c5b848d37cd42423f216be8
ElvishJerricco <elvishjerricco@gmail.com>
2014-09-29 16:04:36 -0400

    insert and delete for stringbuilder

--- CCLib/src/java/lang/StringBuilder.java
+++ CCLib/src/java/lang/StringBuilder.java
@@ -63,6 +63,73 @@ public class StringBuilder {
 		return append(Double.valueOf(d));
 	}
 
+	public StringBuilder insert(int offset, Character[] arr) {
+		for (Character c : arr) {
+			string.add(offset++, c);
+		}
+		return this;
+	}
+
+	public StringBuilder insert(int offset, char[] arr) {
+		Character[] ch = new Character[arr.length];
+		for (int i = 0; i < arr.length; ++i) {
+			ch[i] = arr[i];
+		}
+		return insert(offset, ch);
+	}
+
+	public StringBuilder insert(int offset, String s) {
+		return insert(offset, s.toCharArray());
+	}
+
+	public StringBuilder insert(int offset, Object o) {
+		return insert(offset, o.toString());
+	}
+
+	public StringBuilder insert(int offset, boolean b) {
+		return insert(offset, Boolean.valueOf(b));
+	}
+
+	public StringBuilder insert(int offset, char c) {
+		return insert(offset, Character.valueOf(c));
+	}
+
+	public StringBuilder insert(int offset, int i) {
+		return insert(offset, Integer.valueOf(i));
+	}
+
+	public StringBuilder insert(int offset, long lng) {
+		return insert(offset, Long.valueOf(lng));
+	}
+
+	public StringBuilder insert(int offset, float f) {
+		return insert(offset, Float.valueOf(f));
+	}
+
+	public StringBuilder insert(int offset, double d) {
+		return insert(offset, Double.valueOf(d));
+	}
+
+	public StringBuilder delete(int start, int end) {
+		for (int i = start; i < end; ++i) {
+			string.remove(start);
+		}
+
+		return this;
+	}
+
+	public String substring(int start) {
+		return substring(start, length());
+	}
+
+	public String substring(int start, int end) {
+		char[] c = new char[end - start];
+		for (int i = start; i < end; ++i) {
+			c[i - start] = string.get(i);
+		}
+		return new String(c);
+	}
+
 	public String toString() {
 		char[] arr = new char[string.size()];
 		for (int i = 0; i < arr.length; ++i) {
