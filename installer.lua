efda14d6b1a5c0986c370f50401d7f6608c2a630
ElvishJerricco <elvishjerricco@gmail.com>
2014-09-30 02:08:28 -0400

    Fixed installer

--- installer.lua
+++ installer.lua
@@ -2,6 +2,6 @@ local function grin(...)
 	@GRIN@
 end
 
-grin("-u", "Yevano", "-r", "JVML-JIT")
+grin("-u", "Yevano", "-r", "JVML-JIT", "jvml")
 
 print("It is recommended that you add jvml/bin to your shell's path.")
