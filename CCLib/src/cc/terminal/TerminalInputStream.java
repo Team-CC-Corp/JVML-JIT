6bf320ea80c04146969dd46690f7cc3b6e5507d1
ElvishJerricco <elvishjerricco@gmail.com>
2014-10-06 01:08:44 -0400

    BufferedInputStream
    Issues that made this not work before:
    
    TerminalInputStream should have been overriding the byte array read
    method to only fill until '\n'.
    
    The iand instruction was incorrect, leading to the & operator always
    returning the right hand side parameter.

--- CCLib/src/cc/terminal/TerminalInputStream.java
+++ CCLib/src/cc/terminal/TerminalInputStream.java
@@ -39,4 +39,34 @@ public class TerminalInputStream extends InputStream {
         index++;
         return c;
     }
+
+    @Override
+    public int read(byte b[], int off, int len) throws IOException {
+        if (b == null) {
+            throw new NullPointerException();
+        } else if (off < 0 || len < 0 || len > b.length - off) {
+            throw new IndexOutOfBoundsException();
+        } else if (len == 0) {
+            return 0;
+        }
+
+        int c = read();
+        if (c == -1) {
+            return -1;
+        }
+        b[off] = (byte) c;
+
+        int i = 1;
+        try {
+            for (; i < len && c != '\n'; i++) {
+                c = read();
+                if (c == -1) {
+                    break;
+                }
+                b[off + i] = (byte) c;
+            }
+        } catch (IOException ee) {
+        }
+        return i;
+    }
 }
