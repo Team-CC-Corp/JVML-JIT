4c49a7af9c73e901627207102364945986da6a66
ElvishJerricco <elvishjerricco@gmail.com>
2014-12-28 23:48:14 -0500

    System properties

--- CCLib/src/java/lang/System.java
+++ CCLib/src/java/lang/System.java
@@ -3,6 +3,7 @@ package java.lang;
 import java.io.InputStream;
 import java.io.BufferedInputStream;
 import java.io.PrintStream;
+import java.util.HashMap;
 
 import cc.terminal.TerminalInputStream;
 import cc.terminal.TerminalOutputStream;
@@ -15,7 +16,13 @@ public final class System {
     public static PrintStream err;
     public static InputStream in;
 
+    private static HashMap<String, String> props;
+    private static native void initProperties();
+
     static {
+        props = new HashMap<>();
+        initProperties();
+
         term = new SystemTerminal();
         out = new PrintStream(new TerminalOutputStream(term), true);
         TerminalOutputStream errTerm = new TerminalOutputStream(term);
@@ -27,4 +34,36 @@ public final class System {
     native public static void load(String nativeName);
 
     public static native void arraycopy(Object src, int srcPos, Object dest, int destPos, int length);
+
+    public static String getProperty(String key) {
+        checkKey(key);
+        return props.get(key);
+    }
+
+    public static String getProperty(String key, String def) {
+        checkKey(key);
+        String value = props.get(key);
+        if (value == null)
+            return def;
+        return value;
+    }
+
+    public static String setProperty(String key, String value) {
+        checkKey(key);
+        return props.put(key, value);
+    }
+
+    public static String clearProperty(String key) {
+        checkKey(key);
+        return props.remove(key);
+    }
+
+    private static void checkKey(String key) {
+        if (key == null) {
+            throw new NullPointerException("key can't be null");
+        }
+        if (key.equals("")) {
+            throw new IllegalArgumentException("key can't be empty");
+        }
+    }
 }
--- CCLib/src/java/lang/native/System.lua
+++ CCLib/src/java/lang/native/System.lua
@@ -12,3 +12,20 @@ natives["java.lang.System"]["arraycopy(Ljava/lang/Object;ILjava/lang/Object;II)V
         dest[5][i + destPos] = src[5][i + srcPos]
     end
 end
+
+natives["java.lang.System"]["initProperties()V"] = function()
+    local defaultProps = {
+        ["file.separator"]  = "/"
+        ["line.separator"]  = "\n"
+        ["path.separator"]  = ":"
+        ["user.dir"      ]  = shell.dir()
+    }
+
+    local System = classByName("java.lang.System")
+    local propsMap = System.fields[System.fieldIndexByName["props"]]
+    local put = findMethod(propsMap[1], "put(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;")[1]
+
+    for k,v in pairs(defaultProps) do
+        put(propsMap, toJString(tostring(k)), toJString(tostring(v)))
+    end
+end
